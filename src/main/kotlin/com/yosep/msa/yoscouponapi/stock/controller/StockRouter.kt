5f2326fd43c11e7f69092a00b51e3737f8867d5e
yosepjeon <enekelx1@naver.com>
2020-10-29 14:27:27 +0900

    webflux 기반 reactive redis 기능 추가

--- src/main/kotlin/com/yosep/msa/yoscouponapi/stock/controller/StockRouter.kt
+++ src/main/kotlin/com/yosep/msa/yoscouponapi/stock/controller/StockRouter.kt
@@ -1,12 +1,22 @@
 package com.yosep.msa.yoscouponapi.stock.controller
 
+import com.yosep.msa.yoscouponapi.stock.service.StockHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.RequestPredicates.POST
+import org.springframework.web.reactive.function.server.RequestPredicates.path
 import org.springframework.web.reactive.function.server.RouterFunctions.nest
+import org.springframework.web.reactive.function.server.router
+
 
 @Configuration
-class StockRouter {
+class StockRouter(private val handler: StockHandler) {
     @Bean
-    fun routerFunction() = nest("/api/coupon-stock"),
-        rou
+    fun routerFunction() = nest(path("/api/coupon-stock"),
+        router {
+            listOf(
+                    POST("/{id}",handler::haveRestCoupon)
+            )
+        }
+    )
 }
